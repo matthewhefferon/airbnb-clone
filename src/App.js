ac40f21ee46c99d6e7180f44d3aa932300eb7552
Matthew Hefferon <matthewhefferon@Matthews-MacBook-Air.local>
2020-10-21 20:20:55 -0600

    big commit with firebase hosting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import Home from "./components/Home";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import SearchPage from "./components/SearchPage";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <h1>airbnb clone</h1>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
